394884b9e6fa8315a11ff4aff5bf65d403622c4d
Jacob Latonis <latonis@wisc.edu>
2022-05-01 18:19:39 -0500

    working through Kicad

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # weather-station
+
+This repoistory holds my current project for designing a home monitoring solution for temperature, humidity, and eventually the status of the plants in my office. Eventually, I would like to use an ESP32 or similar Wi-Fi enabled chip to host a small webserver or send notifications to alert me when desired specifications are met.
+
+## Features
+- measure temperature in Fahrenheit
+- measure humidity
+- display these measurements visually
+
+## Goal Features
+- measure soil moisture
+- alert when soil is too dry
+- alert when temperature is below a set value
+- alert when temperature is above a set value
+- alert when humidity is above a set value
+
+# Parts List (continually updated)
+- Arduino Nano (Atmega 168P)
+- LCD1602 Module
+- DHT11 Sensor
+- Many jumper wires (for the time being)
+
+# Schematic
+All of the schematics can be found in the [`sch/`](sch/) directory.
+
+# Code
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
+#include <stdio.h>
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include "DHT.h"
-#include <stdio.h>
+
 
 
 int main(int argc, char **argv) {
   init();
   const int dhtPin = 12;
+  const int backlight = 7;
   const int rs = 11, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
   char buffer[20];
-
+  pinMode(backlight, OUTPUT); 
+  analogWrite(backlight, 128);
   DHT dht(dhtPin, DHT11);
   LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -20,11 +23,11 @@ int main(int argc, char **argv) {
     lcd.clear();
     float humidity = dht.readHumidity();
     float temp = dht.readTemperature(true);
-    snprintf(buffer, sizeof(buffer), "Humidity: %d", (int)humidity);
+    snprintf(buffer, sizeof(buffer), "Humidity: %d%%", (int)humidity);
 
     lcd.begin(16, 2);
     lcd.print(buffer);
-    snprintf(buffer, sizeof(buffer), "Temperature: %d", (int)temp);
+    snprintf(buffer, sizeof(buffer), "Temperature: %dF", (int)temp);
     lcd.setCursor(0,1);
     lcd.print(buffer);
     delay(5000);
