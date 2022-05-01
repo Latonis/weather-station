875401c1358ef735bc70baf2d9f131bd1136fa66
Jacob Latonis <latonis@wisc.edu>
2022-05-01 13:14:15 -0500

    lcd beginning

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 #include "DHT.h"
+#include <stdio.h>
+
 
 int main(int argc, char **argv) {
   init();
+  const int dhtPin = 12;
+  const int rs = 11, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
-  DHT dht(6, DHT11);
+  DHT dht(dhtPin, DHT11);
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
   dht.begin();
-  Serial.begin(9600);
 
   for(;;) {
     float humidity = dht.readHumidity();
@@ -14,7 +20,6 @@ int main(int argc, char **argv) {
 
     Serial.print("Humidity:\t"); Serial.println(humidity);
     Serial.print("Temperature:\t"); Serial.println(temp);
-    Serial.println("=========\n");
     delay(5000);
   }
 
