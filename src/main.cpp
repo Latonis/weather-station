8b242057bff1658f4dfd358d9a652190d8480062
Jacob Latonis <latonis@wisc.edu>
2022-05-01 12:53:15 -0500

    temperature working

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <Arduino.h>
+#include "DHT.h"
 
 int main(int argc, char **argv) {
   init();
-  
-  pinMode(6, OUTPUT);
+
+  DHT dht(6, DHT11);
+  dht.begin();
   Serial.begin(9600);
 
   for(;;) {
-    digitalWrite(6, HIGH);
-    delay(1000);
-    digitalWrite(6, LOW);
-    delay(1000);
+    float humidity = dht.readHumidity();
+    float temp = dht.readTemperature(true);
+
+    Serial.print("Humidity:\t"); Serial.println(humidity);
+    Serial.print("Temperature:\t"); Serial.println(temp);
+    Serial.println("=========\n");
+    delay(5000);
   }
 
 }
